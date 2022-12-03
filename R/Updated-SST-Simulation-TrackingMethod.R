3c78c296efc81f42a1a4ab9668042ec7ef34953f
imstatsbee <imstatsbee@gmail.com>
2022-12-03 16:46:31 -0700

    update

--- R/Updated-SST-Simulation-TrackingMethod.R
+++ R/Updated-SST-Simulation-TrackingMethod.R
@@ -45,116 +45,185 @@ library(dplyr)
 #' ## Example3
 #' ## This example is expected to created an error
 #' ##
-#' pid="Jane.Smith"
-#' n=50
-#' m=10
-#' SSD.b=200
-#' dist.go="LN"
-#' theta.go=c(100,0.01,50)
-#' dist.stop="SW"
-#' theta.stop=c(75,0.01,100)
-#' myblockSSTdata3<-SstSimulatedTrackingMethodBlock(pid,n,m,SSD.b,dist.go,theta.go,dist.stop,theta.stop)
-
+#' myblockSSTdata3 <- SstSimulatedTrackingMethodBlock(
+#' pid="Jane.Smith", n=50, m=10, SSD.b=200, dist.go="LN",
+#' theta.go=c(100,0.01,50), dist.stop="SW", theta.stop=c(75,0.01,100))
+#' myblockSSTdata3
 #'
 #'
 #' @export
 
-SstSimulatedTrackingMethodBlock=function(pid,n,m,SSD.b,dist.go,theta.go,dist.stop,theta.stop){
+SstSimulatedTrackingMethodBlock <- function(pid, n, m, SSD.b,
+                                            dist.go, theta.go,
+                                            dist.stop, theta.stop)
+{
 
-  SRRT0=-999;
-  index<-as.vector(matrix(0,nrow=1,ncol = n));
-  id<-as.vector(matrix(pid,nrow=1,ncol = n));
+  SRRT0 <- -999
+  index <- as.vector(matrix(0,nrow=1,ncol = n))
+  id <- as.vector(matrix(pid,nrow=1,ncol = n))
 
-  if(!(dist.go %in% c("ExG", "SW"))) {warning("This is incorrect Go distribution!") & break}
-  if(!(dist.stop %in% c("ExG", "SW"))) {warning("This is incorrect Stop distribution!") & break}
-
-  if(dist.go=="ExG" & dist.stop=="ExG"){
-    GORT  <- round(rexGAUS(n, mu = theta.go[1], sigma = theta.go[2] , nu = theta.go[3]), digits = 1);
-    SSRT  <- round(rexGAUS(n, mu = theta.stop[1], sigma = theta.stop[2], nu = theta.stop[3]), digits = 1)}
-
-  if(dist.go=="ExG" & dist.stop=="SW"){
-    GORT  <- round(rexGAUS(n, mu = theta.go[1], sigma = theta.go[2] , nu = theta.go[3]), digits = 1);
-    SSRT  <- round(rIG(n, mu = theta.stop[1], sigma = theta.stop[2])+ theta.stop[3], digits = 1)}
-
-  if(dist.go=="SW" & dist.stop=="ExG"){
-    GORT  <- round(rIG(n, mu = theta.go[1], sigma = theta.go[2])+ theta.go[3], digits = 1) ;
-    SSRT  <- round(rexGAUS(n, mu = theta.stop[1], sigma = theta.stop[2], nu = theta.stop[3]), digits = 1)}
-
-  if(dist.go=="SW" & dist.stop=="SW"){
-    GORT  <- round(rIG(n, mu = theta.go[1], sigma = theta.go[2])+ theta.go[3], digits = 1) ;
-    SSRT  <- round(rIG(n, mu = theta.stop[1], sigma = theta.stop[2])+ theta.stop[3], digits = 1)}
-
-
-  SSD <-as.vector(matrix(0 ,nrow=1,ncol = n));
-  SSD[1]<-SSD.b;
-  SRRT<-as.vector(matrix(SRRT0,nrow=1,ncol=n));
-
-  TrialType <-as.vector(matrix('Go',nrow=1,ncol = n));
-  Inhibition <-as.vector(matrix('-999',nrow=1,ncol = n));
-
-  #First: Check the fist stop trial#
-
-  if (GORT[1] > SSRT[1]+SSD[1]) { TrialType[1]<- 'Stop(Successful)' } else  { TrialType[1]<- 'Stop(Failed)' }
-  if (GORT[1] > SSRT[1]+SSD[1]) { Inhibition[1]<- '1' } else  { Inhibition[1]<- '0' }
-  if (GORT[1] > SSRT[1]+SSD[1]) {  index[1]<-  +50 } else  { index[1]<- -50}
-  if (GORT[1] > SSRT[1]+SSD[1]) {  SSD[2]<-  SSD[1]+50 } else  { SSD[2]<- SSD[1]-50}
-
-  #Second: Use information above to create a martingale of SSDs !#
-
-  for (i in 1:(m-1))
-  {
-    if (index[i]== 50)  { SSD[i+1]<- SSD[i]+50 }
-    if (index[i]== 50)  {if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) { TrialType[i+1]<- 'Stop(Successful)' }
-      else  {TrialType[i+1]<- 'Stop(Failed)'}  }
-    if (index[i]== 50)  {if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {  index[i+1]<- +50        }
-      else  { index[i+1]<- -50       }  }
-
-    if (index[i]== -50) { SSD[i+1]<- SSD[i]-50 }
-    if (index[i]== -50) { if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) { TrialType[i+1]<- 'Stop(Successful)' }
-      else  {TrialType[i+1]<- 'Stop(Failed)'  } }
-    if (index[i]== -50) { if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {  index[i+1]<- +50        }
-      else  { index[i+1]<- -50         } }
-
-    if (index[i]== 50)  {if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) { Inhibition[i+1]<- '1'}
-      else  {Inhibition[i+1]<- '0'} }
-    if (index[i]== -50) {if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) { Inhibition[i+1]<- '1'}
-      else  {Inhibition[i+1]<- '0'} }
+  if(!(dist.go %in% c("ExG", "SW"))) {
+    warning("This is incorrect Go distribution!") & break
+  }
+  if(!(dist.stop %in% c("ExG", "SW"))) {
+    warning("This is incorrect Stop distribution!") & break
   }
 
+  if(dist.go=="ExG" & dist.stop=="ExG") {
+    GORT  <- round(rexGAUS(n, mu = theta.go[1], sigma = theta.go[2] , nu = theta.go[3]), digits = 1)
+    SSRT  <- round(rexGAUS(n, mu = theta.stop[1], sigma = theta.stop[2], nu = theta.stop[3]), digits = 1)
+  }
 
-  #Third: Update Final Values of SSD given values of Index !#
+  if(dist.go=="ExG" & dist.stop=="SW") {
+    GORT  <- round(rexGAUS(n, mu = theta.go[1], sigma = theta.go[2] , nu = theta.go[3]), digits = 1)
+    SSRT  <- round(rIG(n, mu = theta.stop[1], sigma = theta.stop[2])+ theta.stop[3], digits = 1)
+  }
 
+  if(dist.go=="SW" & dist.stop=="ExG") {
+    GORT  <- round(rIG(n, mu = theta.go[1], sigma = theta.go[2])+ theta.go[3], digits = 1)
+    SSRT  <- round(rexGAUS(n, mu = theta.stop[1], sigma = theta.stop[2], nu = theta.stop[3]), digits = 1)
+  }
+
+  if(dist.go=="SW" & dist.stop=="SW") {
+    GORT  <- round(rIG(n, mu = theta.go[1], sigma = theta.go[2])+ theta.go[3], digits = 1)
+    SSRT  <- round(rIG(n, mu = theta.stop[1], sigma = theta.stop[2])+ theta.stop[3], digits = 1)
+  }
+
+  SSD <- as.vector(matrix(0 ,nrow=1,ncol = n))
+  SSD[1] <- SSD.b
+  SRRT <- as.vector(matrix(SRRT0,nrow=1,ncol=n))
+
+  TrialType <- as.vector(matrix('Go',nrow=1,ncol = n))
+  Inhibition <- as.vector(matrix('-999',nrow=1,ncol = n))
+
+  ##
+  ## First: Check the fist stop trial
+  ##
+
+  if (GORT[1] > SSRT[1]+SSD[1]) {
+    TrialType[1]<- 'Stop(Successful)'
+  } else  {
+    TrialType[1]<- 'Stop(Failed)'
+  }
+  if (GORT[1] > SSRT[1]+SSD[1]) {
+    Inhibition[1]<- '1'
+  } else  {
+    Inhibition[1]<- '0'
+  }
+  if (GORT[1] > SSRT[1]+SSD[1]) {
+    index[1]<-  +50
+  } else  {
+    index[1]<- -50
+  }
+  if (GORT[1] > SSRT[1]+SSD[1]) {
+    SSD[2]<-  SSD[1]+50
+  } else  {
+    SSD[2]<- SSD[1]-50
+  }
+
+  ##
+  ## Second: Use information above to create a martingale of SSDs !
+  ##
+
+  for (i in 1:(m-1)) {
+    if (index[i]== 50)  {
+      SSD[i+1]<- SSD[i]+50
+    }
+    if (index[i]== 50)  {
+      if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {
+        TrialType[i+1]<- 'Stop(Successful)'
+      } else  {
+        TrialType[i+1]<- 'Stop(Failed)'
+      }
+    }
+    if (index[i]== 50)  {
+      if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {
+        index[i+1]<- +50
+      } else {
+        index[i+1]<- -50
+      }
+    }
+
+    if (index[i]== -50) {
+      SSD[i+1]<- SSD[i]-50
+    }
+    if (index[i]== -50) {
+      if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {
+        TrialType[i+1]<- 'Stop(Successful)'
+      } else  {
+        TrialType[i+1]<- 'Stop(Failed)'
+      }
+    }
+    if (index[i]== -50) {
+      if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {
+        index[i+1]<- +50
+      } else  {
+        index[i+1]<- -50
+      }
+    }
+
+    if (index[i]== 50)  {
+      if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {
+        Inhibition[i+1]<- '1'
+      } else  {
+        Inhibition[i+1]<- '0'
+      }
+    }
+    if (index[i]== -50) {
+      if (GORT[i+1] > SSRT[i+1]+SSD[i+1]) {
+        Inhibition[i+1]<- '1'
+      } else  {
+        Inhibition[i+1]<- '0'
+      }
+    }
+  }
+
+  ##
+  ## Third: Update Final Values of SSD given values of Index !
+  ##
 
   SSRTSSD <- as.vector(matrix(0,nrow=1,ncol =n))
 
-  for (i in 1:n)
-  {
+  for (i in 1:n) {
     SSRTSSD[i] <- SSRT[i] + SSD[i]
   }
 
-  #Fourth: We clear unwanted data for Go trials !#
+  ##
+  ## Fourth: We clear unwanted data for Go trials !
+  ##
 
-  for (i in (m+1):n)
-  {
-    if (SSRT[i]!= 'NA') {SSRT[i]= -999}
-    if (SSRTSSD[i]!= 'NA') {SSRTSSD[i]= -999}
-    if (SSD[i] != 'NA')  {SSD[i]= -999}
-    if (index[i] != 'NA') {index[i]= -999}
+  for (i in (m+1):n) {
+    if (SSRT[i]!= 'NA') {
+      SSRT[i]= -999
+    }
+    if (SSRTSSD[i]!= 'NA') {
+      SSRTSSD[i]= -999
+    }
+    if (SSD[i] != 'NA')  {
+      SSD[i]= -999
+    }
+    if (index[i] != 'NA') {
+      index[i]= -999
+    }
   }
 
-  SRRT<-ifelse(TrialType %in% c('Stop(Failed)'), GORT, SRRT0);
-  GORT<-ifelse(TrialType %in% c('Stop(Successful)','Stop(Failed)'), -999, GORT);
+  SRRT <- ifelse(TrialType %in% c('Stop(Failed)'), GORT, SRRT0)
+  GORT <- ifelse(TrialType %in% c('Stop(Successful)','Stop(Failed)'), -999, GORT)
 
-  TrialTypee<-recode(TrialType, 'Stop(Successful)' = "Stop", 'Stop(Failed)' = "Stop", 'Go' = "Go");
-  MAT2=matrix(c(id , TrialTypee, Inhibition , GORT , SSRT ,SRRT , SSD ), nrow=length(GORT ));
+  TrialTypee <- recode(TrialType, 'Stop(Successful)' = "Stop", 'Stop(Failed)' = "Stop", 'Go' = "Go")
+  MAT2 <- matrix(c(id , TrialTypee, Inhibition , GORT , SSRT ,SRRT , SSD ), nrow=length(GORT ))
 
-  MAT22<-matrix(NA,ncol = 7, nrow=length(GORT ));
-  for(i in 1:m){ MAT22[2*i-1,]<-MAT2[i,]; MAT22[2*i,]<-MAT2[m+i,]}
-  for(i in (2*m+1):n) {MAT22[i,]<-MAT2[i,]}
+  MAT22 <- matrix(NA,ncol = 7, nrow=length(GORT ))
+  for (i in 1:m){
+    MAT22[2*i-1,] <- MAT2[i,];
+    MAT22[2*i,] <- MAT2[m+i,]
+  }
+  for(i in (2*m+1):n) {
+    MAT22[i,] <- MAT2[i,]
+  }
 
-  colnames(MAT22)<- c( 'Participant.id', 'Trial','Inhibition', 'GORT', 'SSRT', 'SRRT','SSD');
+  colnames(MAT22) <- c( 'Participant.id', 'Trial','Inhibition', 'GORT', 'SSRT', 'SRRT','SSD');
   return(MAT22)
-
 }
 
 

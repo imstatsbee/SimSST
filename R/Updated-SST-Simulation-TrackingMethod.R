b01d173cd3b992e83ee2b4b0ab63e07165cff7cc
imstatsbee <imstatsbee@gmail.com>
2022-12-03 19:19:54 -0700

    update

--- R/Updated-SST-Simulation-TrackingMethod.R
+++ R/Updated-SST-Simulation-TrackingMethod.R
@@ -23,6 +23,8 @@ library(dplyr)
 #' @param theta.stop c(mu.stop,sigma.stop,tau.stop)
 #' @param SSD.b one block starting stop signal delay
 #'
+#' @returns
+#'
 #' @examples
 #' ##
 #' ## Example1
@@ -232,26 +234,69 @@ SstSimulatedTrackingMethodBlock <- function(pid, n, m, SSD.b,
 ################################################################################
 #' @title
 #' @description
+#' [2] Step(2): Do for b blocks and ExG/SW distribution
+#' (for either Go/Stop)   OK
+#' block: a block name vector of size b blocks
 #'
-#[2] Step(2): Do for b blocks and ExG/SW distribution   (for either Go/Stop)   OK
+#' @param pid: a vector of size b of Participant.id
+#' @param n :a vector of size b of total number of trials
+#' @param m : a vector of size b of total number of stops
+#' @param dist.go:  a vector of size b of   distribution of go trials    (ExG or SW)
+#' @param dist.stop: a vector of size b of   distribution of stop.trials  (ExG or SW)
+#' @param theta.go=c(mu.go,sigma.go,tau.go)   a b*3 matrix
+#' @param theta.stop=c(mu.stop,sigma.stop,tau.stop)   a b*3 matrix
+#'
+#' @details SSD.b  a vector of size b of starting stop signal delay
 
-#block: a block name vector of size b blocks
+#' @returns Output a giant matrix with "sum(n)" rows and (7+1) columns
+#' #Example1
+#' pid<-c("John.Smith","Jane.McDonald","Jane.McDonald")
+#' block<-c(1,1,2)
+#' n<-c(50,100,150)
+#' m<-c(10,20,30)
+#' SSD.b<-c(200,220,240)
+#' dist.go=c("ExG","ExG","ExG")
+#' theta.go= as.matrix.data.frame(rbind(c(400,60,30),c(440,90,90),c(440,90,90)))
+#' dist.stop=c("ExG","ExG","ExG")
+#' theta.stop=as.matrix.data.frame(rbind(c(100,70,60),c(120,80,70),c(120,80,70)))
+#' mySSTdata1<-SstSimulatedTrackingMethod(pid,n,m,SSD.b,dist.go,theta.go,dist.stop,theta.stop)
+#'
+#'  #Example2
+#'  pid<-c("John.Smith","Jane.McDonald","Jane.McDonald")
+#'  block<-c(1,1,2)
+#'  n<-c(50,100,150)
+#'  m<-c(10,20,30)
+#'  SSD.b<-c(200,220,240)
+#'  dist.go=c("ExG","ExG","ExG")
+#'  theta.go= as.matrix.data.frame(rbind(c(400,60,30),c(440,90,90),c(440,90,90)))
+#'  dist.stop=c("SW","SW","SW")
+#'  theta.stop=as.matrix.data.frame(rbind(c(75,0.01,100),c(75,0.01,100),c(75,0.01,100)))
+#'
+#'   mySSTdata2 <- SstSimulatedTrackingMethod(pid,n,m,SSD.b,dist.go,theta.go,dist.stop,theta.stop)
+#'
+#'
+#'
+#'  #
+#'  # Example3  <--- produce error !
+#'  #
+#'  pid<-c("John.Smith")
+#'  block<-c(1)
+#'  n<-c(50)
+#'  m<-c(10)
+#'  SSD.b<-c(200)
+#'  dist.go=c("LN")
+#'  theta.go= as.matrix.data.frame(rbind(c(400,60,30)))
+#'  dist.stop=c("SW")
+#'  theta.stop=as.matrix.data.frame(rbind(c(75,0.01,100)))
+#'
+#'  mySSTdata3 <- SstSimulatedTrackingMethod(pid, n, m, SSD.b, dist.go, theta.go, dist.stop, theta.stop)
+#'
+#'
+#' @export
+#'
 
-#pid: a vector of size b of Participant.id
-#n :a vector of size b of total number of trials
-#m : a vector of size b of total number of stops
-
-#dist.go:  a vector of size b of   distribution of go trials    (ExG or SW)
-#dist.stop: a vector of size b of   distribution of stop.trials  (ExG or SW)
-
-#theta.go=c(mu.go,sigma.go,tau.go)   a b*3 matrix
-#theta.stop=c(mu.stop,sigma.stop,tau.stop)   a b*3 matrix
-
-#SSD.b:  a vector of size b of starting stop signal delay
-
-#Output: a giant matrix with "sum(n)" rows and (7+1) columns
-
-SstSimulatedTrackingMethod <- function(pid, n, m, SSD.b, dist.go, theta.go, dist.stop, theta.stop) {
+SstSimulatedTrackingMethod <- function(pid, n, m, SSD.b, dist.go, theta.go,
+                                       dist.stop, theta.stop) {
 
   b<-length(block)
   csn<-c(0,cumsum(n))
@@ -266,53 +311,6 @@ SstSimulatedTrackingMethod <- function(pid, n, m, SSD.b, dist.go, theta.go, dist
   return(M22);
 }
 
-#Example1
-pid<-c("John.Smith","Jane.McDonald","Jane.McDonald")
-block<-c(1,1,2)
-n<-c(50,100,150)
-m<-c(10,20,30)
-SSD.b<-c(200,220,240)
-dist.go=c("ExG","ExG","ExG")
-theta.go= as.matrix.data.frame(rbind(c(400,60,30),c(440,90,90),c(440,90,90)))
-dist.stop=c("ExG","ExG","ExG")
-theta.stop=as.matrix.data.frame(rbind(c(100,70,60),c(120,80,70),c(120,80,70)))
-
-mySSTdata1<-SstSimulatedTrackingMethod(pid,n,m,SSD.b,dist.go,theta.go,dist.stop,theta.stop)
-
-if(FALSE){
-  #Example2
-  pid<-c("John.Smith","Jane.McDonald","Jane.McDonald")
-  block<-c(1,1,2)
-  n<-c(50,100,150)
-  m<-c(10,20,30)
-  SSD.b<-c(200,220,240)
-  dist.go=c("ExG","ExG","ExG")
-  theta.go= as.matrix.data.frame(rbind(c(400,60,30),c(440,90,90),c(440,90,90)))
-  dist.stop=c("SW","SW","SW")
-  theta.stop=as.matrix.data.frame(rbind(c(75,0.01,100),c(75,0.01,100),c(75,0.01,100)))
-
-  mySSTdata2 <- SstSimulatedTrackingMethod(pid,n,m,SSD.b,dist.go,theta.go,dist.stop,theta.stop)
-}
-
-
-if(FALSE){
-
-  #
-  # Example3  <--- produce error !
-  #
-  pid<-c("John.Smith")
-  block<-c(1)
-  n<-c(50)
-  m<-c(10)
-  SSD.b<-c(200)
-  dist.go=c("LN")
-  theta.go= as.matrix.data.frame(rbind(c(400,60,30)))
-  dist.stop=c("SW")
-  theta.stop=as.matrix.data.frame(rbind(c(75,0.01,100)))
-
-  mySSTdata3 <- SstSimulatedTrackingMethod(pid, n, m, SSD.b, dist.go, theta.go, dist.stop, theta.stop)
-
-}
 
 ################################################################################
 
